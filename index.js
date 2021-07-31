d96c489100a3694a913060c04bc7bb3024e8ab53
niloy1601107 <niloyf3@gmail.com>
2021-08-01 01:37:30 +0600

    day 1 assignments

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-console.log("hello world");
-var a = 5;
-console.log(a);
-// camel casing
-var userName = "Niloy";
+// Day1 Assignment
+
+//declaring variables using var
+var rollNumber = 1601107;
+var cGPA = 2.70; 
+var studentName = "Fahim Muntasir"; 
+var studentStatus = true; 
+
+console.log(rollNumber,typeof(rollNumber));
+console.log(cGPA, typeof(cGPA));
+console.log(studentName, typeof(studentName));
+console.log(studentStatus, typeof(studentStatus));
+
+//using parseInt
+var newVar = parseInt ("1601107")
+
+console.log(newVar + studentName);
+
+// creating random value
+var randomNumber = Math.random() * 20
+
+if (randomNumber>10){
+    console.log(Math.floor(randomNumber));
+}
+else{
+    console.log("less than 10");
+}
